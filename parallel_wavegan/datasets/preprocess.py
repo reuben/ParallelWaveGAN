9094ad4895c3c36200cbac3188bbb499471f8421
erogol <root@sp-mlc3-5266-0.mlc>
2019-11-18 14:33:30 +0000

    updatedataset preprocess.py

--- parallel_wavegan/datasets/preprocess.py
+++ parallel_wavegan/datasets/preprocess.py
@@ -9,24 +9,12 @@ from collections import Counter
 def split_dataset(items):
     is_multi_speaker = False
     speakers = [item[-1] for item in items]
-    is_multi_speaker = len(set(speakers)) > 1
-    eval_split_size = 500 if len(items) * 0.01 > 500 else int(
+    # max 100 eval samples
+    eval_split_size = 100 if len(items) * 0.01 > 100 else int(
         len(items) * 0.01)
     np.random.seed(0)
     np.random.shuffle(items)
-    if is_multi_speaker:
-        items_eval = []
-        # most stupid code ever -- Fix it !
-        while len(items_eval) < eval_split_size:
-            speakers = [item[-1] for item in items]
-            speaker_counter = Counter(speakers)
-            item_idx = np.random.randint(0, len(items))
-            if speaker_counter[items[item_idx][-1]] > 1:
-                items_eval.append(items[item_idx])
-                del items[item_idx]
-        return items_eval, items
-    else:
-        return items[:eval_split_size], items[eval_split_size:]
+    return items[:eval_split_size], items[eval_split_size:]
 
 
 def load_meta_data(datasets):
@@ -55,21 +43,14 @@ def get_preprocessor_by_name(name):
     return getattr(thismodule, name.lower())
 
 
-def ljspeech(root_path, meta_file):
-    """Normalizes the Nancy meta data file to TTS format"""
-    txt_file = os.path.join(root_path, meta_file)
-    items = []
-    speaker_name = "ljspeech"
-    with open(txt_file, 'r') as ttf:
-        for line in ttf:
-            cols = line.split('|')
-            wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
-            text = cols[1]
-            items.append([wav_file, speaker_name])
-    return items
-
-
 def tts_gen(root_path, meta_file):
+    """
+    Args:
+        root_path: path to root data folder.
+        meta_file: path to metadata file which has
+            audio and feature file paths at each line
+            "/full/path/voice.wav|/full/path/mel.npy"
+    """
     txt_file = os.path.join(root_path, meta_file)
     items = []
     with open(txt_file, 'r') as ttf:
@@ -77,5 +58,6 @@ def tts_gen(root_path, meta_file):
             cols = line.split('|')
             wav_file = cols[0]
             mel_file = cols[1].strip()
-            items.append([wav_file, mel_file])
+            # voice-file, mel-file, speaker-name
+            items.append([wav_file, mel_file, "tts_gen"])
     return items
